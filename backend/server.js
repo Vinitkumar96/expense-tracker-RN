3c1a2f99de61b9241f1beb11877070241239334f
vinit <vinitkumar93341@gmail.com>
2026-02-19 10:45:36 +0530

    create-transactions-api

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,33 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5001
 
+app.use(express.json())
 
+
+app.post("/api/transactions",async(req,res)=>{
+    try{
+        const{title,amount,category,user_id} = req.body;
+
+        if(!title || !amount || !category || !user_id){
+            return res.status(400).json({
+                message:"All fields are required"
+            })
+        }
+
+       const transaction =  await sql`
+        INSERT INTO transactions(user_id,title,amount,category)
+        VALUES (${user_id},${title},${amount},${category})
+        RETURNING *
+        `
+        // console.log(transaction)
+        // console.log(transaction[0])
+        res.status(201).json(transaction[0])
+
+    }catch(error){
+        console.log("Error creating the transaction",error)
+        res.status(500).json({message:"Internal server error"})
+    }
+})
 
 app.get("/",(req,res)=>{
     res.send("working")
