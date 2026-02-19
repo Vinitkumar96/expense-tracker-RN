1b0971721923f23a6b003d8a70a4fe54d6e3f018
vinit <vinitkumar93341@gmail.com>
2026-02-19 11:18:38 +0530

    get trnscs with userid and delete with id

--- backend/server.js
+++ backend/server.js
@@ -1,64 +1,98 @@
-import express from "express"
-import dotenv from "dotenv"
-import { sql } from "./config/db.js"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 5001
+const app = express();
+const port = process.env.PORT || 5001;
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
-app.post("/api/transactions",async(req,res)=>{
-    try{
-        const{title,amount,category,user_id} = req.body;
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
 
-        if(!title || !amount || !category || !user_id){
-            return res.status(400).json({
-                message:"All fields are required"
-            })
-        }
+    if (!title || !amount || !category || !user_id) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
 
-       const transaction =  await sql`
+    const transaction = await sql`
         INSERT INTO transactions(user_id,title,amount,category)
         VALUES (${user_id},${title},${amount},${category})
         RETURNING *
-        `
-        // console.log(transaction)
-        // console.log(transaction[0])
-        res.status(201).json(transaction[0])
+        `;
+    // console.log(transaction)
+    // console.log(transaction[0])
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log("Error creating the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-    }catch(error){
-        console.log("Error creating the transaction",error)
-        res.status(500).json({message:"Internal server error"})
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params
+
+    const transactions = await sql`
+            SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+        `;
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting the transactions", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const {id} = req.params
+    // console.log(typeof id)
+    if(isNaN(parseInt(id))){
+        return res.status(400).json({message:"Invalid transaction ID"})
     }
-})
+    const result = await sql`
+        DELETE FROM transactions WHERE id = ${id} RETURNING *
+    `
+    if(result.length === 0){
+        return res.status(404).json({message: "Transaction not found"})
+    }
 
-app.get("/",(req,res)=>{
-    res.send("working")
-})
+    res.status(200).json({
+        message:"Transaction deleted successfully"
+    })
 
+  } catch (error) {
+    console.log("Error getting the transactions", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 async function initDb() {
-    try{
-       await sql`CREATE TABLE IF NOT EXISTS transactions(
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
         id SERIAL PRIMARY KEY,
         user_id VARCHAR(255) NOT NULL,
         title VARCHAR(255) NOT NULL,
         amount DECIMAL(10,2) NOT NULL,
         category VARCHAR(255) NOT NULL,
         created_at DATE NOT NULL DEFAULT CURRENT_DATE
-       )`
-       console.log('Db initialized successfully')
-    }catch(error){
-        console.log("Error initializing DB",error)
-        process.exit(1)
-    }
+       )`;
+    console.log("Db initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1);
+  }
 }
 
-
-initDb().then(()=>{
-    app.listen(port,()=>{
-        console.log(`server is up and running on port:${port}`)
-    })
-})
+initDb().then(() => {
+  app.listen(port, () => {
+    console.log(`server is up and running on port:${port}`);
+  });
+});
