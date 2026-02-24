40d4aa90d88eecc05787ddb384cf4daa30ac8744
vinit <vinitkumar93341@gmail.com>
2026-02-24 22:03:40 +0530

    useTransaction hook

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,9 +2,13 @@ import { SignOutButton } from '@/components/sign-out-button'
 import { SignedIn, SignedOut, useSession, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { StyleSheet, View,Text } from 'react-native'
+import useTransactions from '../../hooks/useTransactions'
+import { useEffect } from 'react'
+import PageLoader from '../../components/PageLoader'
 
 export default function Page() {
   const { user } = useUser()
+  const {transactions,summary,isLoading, loadData,deleteTransaction}= useTransactions(user.id)
 
   // If your user isn't appearing as signed in,
   // it's possible they have session tasks to complete.
@@ -12,19 +16,21 @@ export default function Page() {
   const { session } = useSession()
   console.log(session?.currentTask)
 
+  useEffect(()=>{
+    loadData()
+  },[loadData])
+
+  // console.log("transactions",transactions);
+  // console.log("summary",summary);
+  // console.log(user.id);
+
+  if(isLoading){
+    return <PageLoader/>
+  }
+
   return (
     <View>
-      {/* <ThemedText type="title">Welcome!</ThemedText> */}
-
-      {/* Show the sign-in and sign-up buttons when the user is signed out */}
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+      
 
       {/* Show the sign-out button when the user is signed in */}
       <SignedIn>
