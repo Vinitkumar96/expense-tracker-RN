e5417b71b1e096c44008c5835f582f1c33e45d63
vinit <vinitkumar93341@gmail.com>
2026-02-27 23:24:57 +0530

    done with create

--- mobile/app/(root)/create.jsx
+++ mobile/app/(root)/create.jsx
@@ -1,8 +1,19 @@
-import { View, Text, Alert} from 'react-native'
-import { API_URL } from '../../constants/api'
-import React, { useState } from 'react'
-import {useRouter} from "expo-router"
-import {useUser} from "@clerk/clerk-expo"
+import {
+  View,
+  Text,
+  Alert,
+  TouchableOpacity,
+  ActivityIndicator,
+  TextInput,
+  Platform,
+} from "react-native";
+import { API_URL } from "../../constants/api";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import { useUser } from "@clerk/clerk-expo";
+import { styles } from "../../assets/styles/create.style";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants/colors";
 
 const CATEGORIES = [
   { id: "food", name: "Food & Drinks", icon: "fast-food" },
@@ -15,66 +26,219 @@ const CATEGORIES = [
 ];
 
 const CreateScreen = () => {
-  const router = useRouter()
-  const {user} = useUser()
+  const router = useRouter();
+  const { user } = useUser();
 
-  const[title,setTitle] = useState("")
-  const[amount,setAmount] = useState("")
-  const[selectedCategory, setSelectedCategory] = useState("")
-  const[isExpense, setIsExpense] = useState(true)
-  const[isLoading, setIsLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [isExpense, setIsExpense] = useState(true);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCreate = async () => {
-    if(!title.trim()){
-      return Alert.alert("Error","Please enter a transaction title")
-    } 
-    if(!amount || isNaN(parseFloat(amount)) || parseFloat(amount) <= 0){
-     return Alert.alert("Error","Please enter a valid amount")
+    if (!title.trim()) {
+      return Alert.alert("Error", "Please enter a transaction title");
     }
-    if(!selectedCategory){
-      return Alert.alert("Error","Please select a category")
+    if (!amount || isNaN(parseFloat(amount)) || parseFloat(amount) <= 0) {
+      return Alert.alert("Error", "Please enter a valid amount");
+    }
+    if (!selectedCategory) {
+      return Alert.alert("Error", "Please select a category");
     }
 
-    setIsLoading(true)
+    setIsLoading(true);
 
-    try{
-      const formatAmount = isExpense ? -Math.abs(parseFloat(amount)) : Math.abs(parseFloat(amount))
+    try {
+      const formatAmount = isExpense
+        ? -Math.abs(parseFloat(amount))
+        : Math.abs(parseFloat(amount));
 
-      const response = await fetch(`${API_URL}/transactions`,{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
+      const response = await fetch(`${API_URL}/transactions`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           user_id: user.id,
           title,
           amount: formatAmount,
-          category: selectedCategory
-        })
-      })
+          category: selectedCategory,
+        }),
+      });
 
-      if(!response.ok){
+      if (!response.ok) {
         const errorData = await response.json();
         console.log(errorData);
-        throw new Error(errorData.error || "Failed to create transaction")
+        throw new Error(errorData.error || "Failed to create transaction");
       }
 
-      Alert.alert("Success","Transaction created successfully")
+      Alert.alert("Success", "Transaction created successfully");
       router.back();
-      
-    }catch(error){
-      Alert.alert("Error",error.message || "Failed to create transaction")
-      console.error("Error creating transaction:", error)
-    }finally{
-      setIsLoading(false)
+    } catch (error) {
+      Alert.alert("Error", error.message || "Failed to create transaction");
+      console.error("Error creating transaction:", error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
-    <View>
-      <Text>create a routine and things to achieve till saturday dsa</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      {/* header */}
+      <View style={styles.header}>
+        {/* back button in left side  */}
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => router.back()}
+        >
+          <Ionicons name="arrow-back" size={24} color={COLORS.text} />
+        </TouchableOpacity>
+        {/* heading in center part */}
+        <Text style={styles.headerTitle}>New Transaction</Text>
+        {/* right side save button */}
+        {/* if loading true then disabled */}
+        <TouchableOpacity
+          style={[
+            styles.saveButtonContainer,
+            isLoading && styles.saveButtonDisabled,
+          ]}
+          onPress={handleCreate}
+          disabled={isLoading}
+        >
+          {/* text on the basis of loading state */}
+          <Text style={styles.saveButton}>
+            {isLoading ? "Saving..." : "Save"}
+          </Text>
+          {!isLoading && (
+            <Ionicons name="checkmark" size={18} color={COLORS.primary} />
+          )}
+        </TouchableOpacity>
+      </View>
 
-export default CreateScreen
+      {/* box or bodyyy */}
+      <View style={styles.card}>
+
+        {/* select expense or income button..... view */}
+        <View style={styles.typeSelector}>
+          <TouchableOpacity
+            style={[styles.typeButton, isExpense && styles.typeButtonActive]}
+            onPress={() => setIsExpense(true)}
+          >
+            <Ionicons
+              name="arrow-down-circle"
+              size={22}
+              color={isExpense ? COLORS.white : COLORS.expense}
+              style={styles.typeIcon}
+            />
+            <Text
+              style={[
+                styles.typeButtonText,
+                isExpense && styles.typeButtonTextActive,
+              ]}
+            >
+              Expense
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.typeButton, !isExpense && styles.typeButtonActive]}
+            onPress={() => setIsExpense(false)}
+          >
+            <Ionicons
+              name="arrow-up-circle"
+              size={22}
+              color={!isExpense ? COLORS.white : COLORS.income}
+              style={styles.typeIcon}
+            />
+            <Text
+              style={[
+                styles.typeButtonText,
+                !isExpense && styles.typeButtonTextActive,
+              ]}
+            >
+              Income
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+          {/* AMOUNT CONTAINER with text input and onchange text  */}
+        <View style={styles.amountContainer}>
+          <Text style={styles.currencySymbol}>$</Text>
+          <TextInput
+            style={styles.amountInput}
+            placeholder="0.00"
+            placeholderTextColor={COLORS.textLight}
+            value={amount}
+            onChangeText={setAmount}
+            keyboardType="numeric"
+          />
+        </View>
+
+         {/* INPUT CONTAINER with text input for transaction */}
+        <View style={styles.inputContainer}>
+          <Ionicons
+            name="create-outline"
+            size={22}
+            color={COLORS.textLight}
+            style={styles.inputIcon}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Transaction Title"
+            placeholderTextColor={COLORS.textLight}
+            value={title}
+            onChangeText={setTitle}
+          />
+        </View>
+
+        {/* select the category section at bottom  */}
+        <Text style={styles.sectionTitle}>
+          <Ionicons name="pricetag-outline" size={16} color={COLORS.text} />{" "}
+          Category
+        </Text>
+
+        <View style={styles.categoryGrid}>
+          {CATEGORIES.map((cat) => (
+            <TouchableOpacity
+              key={cat.id}
+              onPress={() => setSelectedCategory(cat.name)}
+              style={[
+                styles.categoryButton,
+                selectedCategory === cat.name ? COLORS.white : COLORS.text,
+              ]}
+            >
+              <Ionicons
+                name={cat.icon}
+                size={20}
+                color={
+                  selectedCategory === cat.name ? COLORS.white : COLORS.text
+                }
+                style={styles.categoryIcon}
+              />
+              <Text
+                style={[
+                  styles.categoryButtonText,
+                  selectedCategory === cat.name &&
+                    styles.categoryButtonTextActive,
+                ]}
+              >
+                {cat.name}{Platform.OS === "android" && "   "}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      {/* outside the box we will have loader */}
+      {/* importing the loader animation from react native */}
+      {isLoading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default CreateScreen;
